28072f970b3dfdd8639238ed1a64353155ec78a4
Linkhe <kikokvo83@gmail.com>
2024-09-20 23:09:24 -0300

    Removed Config

--- src/main/java/net/actuallylinkhe/justflax/JustFlax.java
+++ src/main/java/net/actuallylinkhe/justflax/JustFlax.java
@@ -9,7 +9,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -38,8 +37,6 @@ public class JustFlax
         MinecraftForge.EVENT_BUS.register(this);
 
         modEventBus.addListener(this::addCreative);
-
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
